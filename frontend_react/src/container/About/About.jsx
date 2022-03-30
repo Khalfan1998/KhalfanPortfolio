dc963cbffd6a14ab26f2a2a86c99e4b71bf886ff
Khalfan1998 <khalfan.ikns@gmail.com>
2022-03-30 15:59:26 +0300

    connecting to sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,16 +3,26 @@ import {motion} from "framer-motion"
 
 import {images} from "../../constants"
 import "./About.scss"
+import {urlFor, client} from "../../client";
 
-const abouts = [
-  { title: "Web Development", description: "I am a good web developer.", imgUrl: images.about01},
-  { title: "Frontend Development", description: "I am a good web developer.", imgUrl:images.about02},
-  { title: "UI/UX", description: "I am a good web developer.", imgUrl:images.about03},
-  { title: "MERN Stack", description: "I am a good web developer.", imgUrl:images.about04},
+// const abouts = [
+//   { title: "Web Development", description: "I am a good web developer.", imgUrl: images.about01},
+//   { title: "Frontend Development", description: "I am a good web developer.", imgUrl:images.about02},
+//   { title: "UI/UX", description: "I am a good web developer.", imgUrl:images.about03},
+//   { title: "MERN Stack", description: "I am a good web developer.", imgUrl:images.about04},
 
-]
+// ]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+   const  query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+    .then((data)=> setAbouts(data))
+  }, []);
+  
   return (
     <>
     <h2 className="head-text">I Know That <span>Good Web Apps</span><br/> means <span>Good Business</span></h2>
@@ -25,7 +35,7 @@ const About = () => {
         className="app__profile-item"
         key={about.title + index}
         >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
             <p className="p-text" style={{marginTop:10}}>{about.description}</p>
 
