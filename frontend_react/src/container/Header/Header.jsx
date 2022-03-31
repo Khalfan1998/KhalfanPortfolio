262906b2bb83b3fc3cf06b439a019da205dd6c5d
Khalfan1998 <khalfan.ikns@gmail.com>
2022-03-31 13:15:29 +0300

    adding skills container

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
             </div>
           </div>
           <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
+          <p className="p-text">Junior Web Developer</p>
           </div>
         </div>
       </motion.div>
